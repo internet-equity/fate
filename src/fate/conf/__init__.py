598de80bd7d92dd79825b67ed2de171575f154c9
Jesse London <jesselondon@gmail.com>
2022-08-01 16:28:50 -0500

    made ConfGroup Spec enum available from conf package interface
    to help in downstream customizations of runtime

--- src/fate/conf/__init__.py
+++ src/fate/conf/__init__.py
@@ -4,6 +4,9 @@ from .base import ConfGroup
 from .error import ConfValueError, MultiConfError, NoConfError  # noqa: F401
 
 
+spec = ConfGroup._Spec
+
+
 @functools.wraps(ConfGroup, assigned=('__doc__', '__annotations__'), updated=())
 def get(*args, **kwargs):
     return ConfGroup(*args, **kwargs)
