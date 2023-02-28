7b57e6f9cdca9401ea37d76398d39082822ea52c
Jesse London <jesselondon@gmail.com>
2023-02-28 13:01:32 -0600

    support for "jenkins-style" hashed cron expressions
    a task schedule cron expression *may* be given an arbitrary offset
    according to a hash of its configured name, using `H` in lieu of `*`
    
    this feature discourages task scheduling overlaps (without explicit
    fine-tuning).
    
    (note that tasks' execution concurrency/multiple-tenancy may be
    configured via the scheduler.)

--- src/fate/conf/types/task.py
+++ src/fate/conf/types/task.py
@@ -1,4 +1,5 @@
 import collections
+import functools
 import logging
 import pathlib
 import re
@@ -96,6 +97,12 @@ class TaskConfType(ConfType):
         return self.__root__.__other__.default
 
     @at_depth(0)
+    @property
+    def _croniter(self):
+        # cover for croniter_range not directly supporting hash_id
+        return functools.partial(croniter.croniter, hash_id=self.__name__)
+
+    @at_depth(0)
     def schedule_iter_(self, t0, t1=None, /, *, max_years_between_matches=None):
         try:
             schedule = self['schedule']
@@ -105,10 +112,10 @@ class TaskConfType(ConfType):
         if not isinstance(schedule, str):
             raise ConfTypeError(self._ScheduleError.bad.self_format(self, schedule))
 
-        runs = (croniter.croniter(schedule, t0,
-                                  max_years_between_matches=max_years_between_matches)
+        runs = (self._croniter(schedule, t0,
+                               max_years_between_matches=max_years_between_matches)
                 if t1 is None
-                else croniter.croniter_range(t0, t1, schedule))
+                else croniter.croniter_range(t0, t1, schedule, _croniter=self._croniter))
 
         try:
             yield from runs
